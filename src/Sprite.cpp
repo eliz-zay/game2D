04ebe401747a444ebc1a78cddb8b499b254ba6e1
Elizaveta Zaydenvarg <liza.zaydenvarg@gmail.com>
2021-02-26 20:39:35 +0300

    Running along walls fixed

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -13,25 +13,25 @@ Sprite::Sprite(glm::vec2 initPosition, std::string textureSource, std::vector<Ti
 void Sprite::move(GLFWwindow* window) {
     const GLfloat EPS = 0.0001;
     float speed = 3.0f;
-    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS && this->currentCoord.y0 > 0 && !this->isCollision(0, -1)) {
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS && this->currentCoord.y0 > 0 && !this->isCollision(0, -speed)) {
         this->transformMatrix = glm::translate(this->transformMatrix, glm::vec3(0.f, -speed * 1.f, 0.f));
         this->currentCoord.y0 -= speed * 1.f;
         this->currentCoord.y1 -= speed * 1.f;
     }
 
-    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS && this->currentCoord.y1 < WINDOW_HEIGHT && !this->isCollision(0, 1)) {
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS && this->currentCoord.y1 < WINDOW_HEIGHT && !this->isCollision(0, speed)) {
         this->transformMatrix = glm::translate(this->transformMatrix, glm::vec3(0.f, speed * 1.f, 0.f));
         this->currentCoord.y0 += speed * 1.f;
         this->currentCoord.y1 += speed * 1.f;
     }
 
-    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS && this->currentCoord.x1 < WINDOW_WIDTH && !this->isCollision(1, 0)) {
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS && this->currentCoord.x1 < WINDOW_WIDTH && !this->isCollision(speed, 0)) {
         this->transformMatrix = glm::translate(this->transformMatrix, glm::vec3(speed * 1.f, 0.f, 0.f));
         this->currentCoord.x0 += speed * 1.f;
         this->currentCoord.x1 += speed * 1.f;
     }
 
-    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS && this->currentCoord.x0 > 0 && !this->isCollision(-1, 0)) {
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS && this->currentCoord.x0 > 0 && !this->isCollision(-speed, 0)) {
         this->transformMatrix = glm::translate(this->transformMatrix, glm::vec3(-speed * 1.f, 0.f, 0.f));
         this->currentCoord.x0 -= speed * 1.f;
         this->currentCoord.x1 -= speed * 1.f;
